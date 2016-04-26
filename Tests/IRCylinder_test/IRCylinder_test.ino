2c8cac788a13f98384074ef0bcfca69bef6631fb
Andreas Magnusson <andreas.magnusson90@gmail.com>
2016-04-26 16:02:57 +0200

    Started test programs for IR cylinder navigation.

---
+++ Tests/IRCylinder_test/IRCylinder_test.ino
@@ -0,0 +1,47 @@
+#include "declarations.h"
+
+void setup() {
+  Serial.begin(9600);                        // Set data rate to 9600 bps
+  Serial.println("BooBoo initiated!"); 
+  //tone(4, 2000,500);
+  //delay(1000);
+  //tone(4, 2000, 500);
+
+  pinMode(irRecPinLeft, INPUT);  pinMode(irLEDPinLeft, OUTPUT);   // IR LED & Receiver
+  pinMode(irRecPinRight, INPUT);  pinMode(irLEDPinRight, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+// IR distance measurement function
+float irDistance(int irLedPin, int irReceivePin)
+{ 
+  float nbrAvgIrRuns = 5.0;
+  float meanDistZone;
+  float sumDistZones = 0.0;
+  
+  for(int k = 1; k<= nbrAvgIrRuns; k++) { 
+    float distance = 0.0;
+    for(long f = irOptFreq; f <= irMaxFreq; f += irFreqStep) {
+      distance += irDetect(irLedPin, irReceivePin, f);
+    }
+    sumDistZones += distance;
+  }
+  meanDistZone = sumDistZones/nbrAvgIrRuns;
+
+  return meanDistZone/nbrIRDistZones; // Normalize to 1
+}
+
+// IR Detection function
+int irDetect(int irLedPin, int irReceiverPin, long frequency)
+{
+  tone(irLedPin, frequency, 10);              // IRLED 38 kHz for at least 1 ms
+  delay(1);                                  // Wait 1 ms
+  int ir = digitalRead(irReceiverPin);       // IR receiver -> ir variable
+  delay(1);                                  // Down time before recheck
+  return ir;                                 // Return 1 no detect, 0 detect
+}    
