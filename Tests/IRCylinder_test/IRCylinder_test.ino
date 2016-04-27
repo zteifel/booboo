cfc4e742eb9fc06485645257baa33a087f55dd81
Andreas Magnusson <andreas.magnusson90@gmail.com>
2016-04-27 09:38:01 +0200

    Minor changes.

--- Tests/IRCylinder_test/IRCylinder_test.ino
+++ Tests/IRCylinder_test/IRCylinder_test.ino
@@ -1,4 +1,8 @@
 #include "declarations.h"
+#include <Servo.h>     
+
+Servo servoLeft;
+Servo servoRight;
 
 void setup() {
   Serial.begin(9600);                        // Set data rate to 9600 bps
@@ -10,10 +14,29 @@ void setup() {
   pinMode(irRecPinLeft, INPUT);  pinMode(irLEDPinLeft, OUTPUT);   // IR LED & Receiver
   pinMode(irRecPinRight, INPUT);  pinMode(irLEDPinRight, OUTPUT);
 
+  servoLeft.attach(13);
+  servoRight.attach(12);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float irDistLeft = irDistance(irLEDPinLeft, irRecPinLeft);         // Measure distance
+  float irDistRight = irDistance(irLEDPinRight, irRecPinRight);
+  digitalWrite(statusLedPinLeft, LOW);
+  digitalWrite(statusLedPinRight, LOW);
+  
+  
+  if(irDistLeft < 0.8){
+      digitalWrite(statusLedPinLeft, HIGH);
+    }
+  
+  if(irDistRight < 0.8){
+       digitalWrite(statusLedPinRight, HIGH); 
+    }
+  
+  Serial.print(irDistLeft);
+  Serial.print("  ");
+  Serial.println(irDistRight); 
 
 }
 
@@ -39,7 +62,7 @@ float irDistance(int irLedPin, int irReceivePin)
 // IR Detection function
 int irDetect(int irLedPin, int irReceiverPin, long frequency)
 {
-  tone(irLedPin, frequency, 10);              // IRLED 38 kHz for at least 1 ms
+  tone(irLedPin, frequency, 10);             // IRLED 38 kHz for at least 1 ms
   delay(1);                                  // Wait 1 ms
   int ir = digitalRead(irReceiverPin);       // IR receiver -> ir variable
   delay(1);                                  // Down time before recheck
