0b8cf8ae1b8cd7b9e8630dcc38a3ea2d77da509e
Andreas Magnusson <andreas.magnusson90@gmail.com>
2016-05-04 16:57:48 +0200

    Started a test main program.

---
+++ Tests/Main_test/Main_test.ino
@@ -0,0 +1,15 @@
+
+#include "../../Libraries/Movement.h"
+#include "../../Libraries/Avoidance.h"
+#include "../../Libraries/IRDistance.h"
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
