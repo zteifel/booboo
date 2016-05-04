325c1742a9ab0a39172a506675bbfa397d5e3edb
Andreas Magnusson <andreas.magnusson90@gmail.com>
2016-05-04 16:58:15 +0200

    Copied library files to main test directory.

---
+++ Tests/Main_test/Movement.h
@@ -0,0 +1,34 @@
+#include "Arduino.h"
+
+#ifndef MOVEMENT_H
+#define MOVEMENT_H
+
+
+void moveForward() {
+  servoLeft.writeMicroseconds(servoLeftForward);
+  servoRight.writeMicroseconds(servoRightForward);
+}
+
+void turnLeft() {
+  servoLeft.writeMicroseconds(servoLeftBackward);
+  servoRight.writeMicroseconds(servoRightForward);
+}
+
+void turnRight() {
+  servoLeft.writeMicroseconds(servoLeftForward);
+  servoRight.writeMicroseconds(servoRightBackward);
+}
+
+void reverse() {
+  servoLeft.writeMicroseconds(servoLeftBackward);
+  servoRight.writeMicroseconds(servoRightBackward);
+}
+
+void moveForwardSlow() {
+    servoLeft.writeMicroseconds(servoLeftFowardSlow);
+    servoRight.wirteMicroseconds(servoRightForwardSlow);
+}
+
+
+
+#endif
