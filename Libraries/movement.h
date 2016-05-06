5da5a10c1bec20baa776440a696d85e267f0a0a7
Andreas Magnusson <andreas.magnusson90@gmail.com>
2016-05-06 10:08:38 +0200

    A version of movement.

--- Libraries/movement.h
+++ Libraries/movement.h
@@ -1,18 +1,34 @@
+#include "Arduino.h"
 
 #ifndef MOVEMENT_H
 #define MOVEMENT_H
 
-#include <Servo.h>
 
-const bool clockwise = true;
-const bool counterClockwise = false;
-const int stepsInFullTurn = 38;
+void moveForward() {
+  servoLeft.writeMicroseconds(servoLeftForward);
+  servoRight.writeMicroseconds(servoRightForward);
+}
 
-void rotateStep(Servo servoLeft, Servo servoRight, bool clockwise);
-void rotate(Servo servoLeft, Servo servoRight, bool clockwise, int speed);
-void stopMovement(Servo servoLeft, Servo servoRight);
-void moveForward(Servo servoLeft, Servo servoRight);
-void moveALittleInRandDir(Servo servoLeft, Servo servoRight);
-void moveALittleInDir(Servo servoLeft, Servo servoRight, int stepsToRotate, bool clockwise, int timeToMove);
+void turnLeft() {
+  servoLeft.writeMicroseconds(servoLeftBackward);
+  servoRight.writeMicroseconds(servoRightForward);
+}
+
+void turnRight() {
+  servoLeft.writeMicroseconds(servoLeftForward);
+  servoRight.writeMicroseconds(servoRightBackward);
+}
+
+void reverse() {
+  servoLeft.writeMicroseconds(servoLeftBackward);
+  servoRight.writeMicroseconds(servoRightBackward);
+}
+
+void moveForwardSlow() {
+    servoLeft.writeMicroseconds(servoLeftFowardSlow);
+    servoRight.wirteMicroseconds(servoRightForwardSlow);
+}
+
+
 
 #endif
