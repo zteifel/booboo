ed25094e0743e2ac3ddc6880c4026654dd1111a2
Victor Bäckman <victorbackman2@gmail.com>
2016-05-16 13:24:15 +0200

    Ir beacon sim

--- Tests/irBeaconSimulator/irBeaconSimulator.ino
+++ Tests/irBeaconSimulator/irBeaconSimulator.ino
@@ -1,18 +1,20 @@
-int irLedPin = 7;
-int frequency = 37500;
+int irLedPin = 2;
+int frequency = 38000;
 int simDelayLimit = 150;
 
 void setup() {
   // put your setup code here, to run once:
 pinMode(irLedPin, OUTPUT);
 pinMode(13,OUTPUT);
+Serial.begin(9600);
+Serial.flush();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
     tone(irLedPin, frequency, 10);              // IRLED 37.5 kHz for at least 1 ms
-    digitalWrite(13,HIGH);
-    delay(simDelayLimit);
-    digitalWrite(13,LOW);
-    delay(simDelayLimit);
+    //digitalWrite(13,HIGH);
+    noTone(irLedPin);
+    //digitalWrite(13,LOW);
+    //delay(simDelayLimit);
 }
